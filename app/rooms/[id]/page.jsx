fe20d24c5ef534754ea45e5bf06365da09b9614f
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-08-26 16:55:40 +0300

    add my rooms

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -7,12 +7,20 @@ import getSingleRoom from '@/app/actions/getSingleRoom';
 
 
 const RoomPage = async ({ params }) => {
-    const { id } = params
+    const { id } = await params
     const room = await getSingleRoom(id);
 
     if (!room) {
         return <Heading title = 'Room not found' />
     }
+
+    const bucketId = process.env.NEXT_PUBLIC_APPWRITE_STORAGE_BUCKET_ROOMS;
+    const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
+  
+    const imageUrl = `https://cloud.appwrite.io/v1/storage/buckets/${bucketId}/files/${room.image}/view?project=${projectId}`;
+  
+    const imageSrc = room.image ? imageUrl : '/images/no-image.jpg';
+
     return ( <>
         <Heading title={room.name} />
         <div className="bg-white shadow rounded-lg p-6">
@@ -26,7 +34,7 @@ const RoomPage = async ({ params }) => {
  
         <div className="flex flex-col sm:flex-row sm:space-x-6">
           <Image
-            src={ `/images/rooms/${room.image}` }
+            src={imageSrc}
             alt={room.name}
             width={400}
             height={100}
